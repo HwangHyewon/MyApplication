b7922e73f7674a925c31d75f479dac3d428e02aa
HwangHyewon <vnfms0123@naver.com>
2017-06-12 17:31:16 +0900

    level1 - 5초마다 text바뀌기 level2 - 5초 후에 글자 안보기에 하기

--- app/src/main/java/com/example/hwanghyewon/myapplication/Level1Activity.java
+++ app/src/main/java/com/example/hwanghyewon/myapplication/Level1Activity.java
@@ -1,20 +1,48 @@
 package com.example.hwanghyewon.myapplication;
 
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
 
+import static com.example.hwanghyewon.myapplication.R.id.textView;
+
+
 public class Level1Activity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
-        TextView textView = (TextView) findViewById(R.id.textView);
+
+        textView = (TextView) findViewById(R.id.textView);
         textView.setText(Integer.toString(makeRandomText()));
+        mHandler.sendEmptyMessage(0);
+
+
     }
+
+    //int answer = Integer.parseInt(editText.getText().toString());
+
+    //5초마다 textview에 보이는 숫자가 바뀐다.
+    Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+
+            textView.setText(Integer.toString(makeRandomText()));
+            mHandler.sendEmptyMessageDelayed(0, 5000);
+
+        }
+    };
+
 
 
     public int makeRandomText() {
--- app/src/main/java/com/example/hwanghyewon/myapplication/Level2Activity.java
+++ app/src/main/java/com/example/hwanghyewon/myapplication/Level2Activity.java
@@ -1,21 +1,42 @@
 package com.example.hwanghyewon.myapplication;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.util.Random;
 
-public class Level2Activity extends AppCompatActivity {
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.RED;
+import static android.graphics.Color.WHITE;
 
+public class Level2Activity extends AppCompatActivity {
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level2);
 
-        TextView textView = (TextView) findViewById(R.id.textView);
+        textView = (TextView) findViewById(R.id.textView);
         textView.setText(Integer.toString(makeRandomText()));
+        mHandler.sendEmptyMessage(0);
+
+        mHandler.postDelayed(runnable,5000);
     }
+
+    //5초뒤에 글자가 안보이게 한다.
+    Handler mHandler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            textView.setTextColor(WHITE);
+            mHandler.sendEmptyMessageDelayed(0,5000);
+
+            };
+
+    };
 
 
     public int makeRandomText() {
